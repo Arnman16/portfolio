0be0029868eea4addf790156bdc64f0885e2a2f9
Aaron Noseworthy <aaron.noseworthy@gmail.com>
2021-06-01 15:33:57 -0230

    fixing footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,11 @@
 <template>
   <v-container class="pa-1" d-flex>
-    
     <v-col>
       <v-row>
-        <v-card
-          color="rgba(255,255,255,0.05)"
-          class="mx-auto ma-1"
-        >
+        <v-card color="rgba(255,255,255,0.05)" class="mx-auto ma-1">
           <v-card-actions flat class="pa-0">
-            <v-btn dark dense v-for="(link, index) in links" :key="index" icon>
-              <v-icon small>{{ link }}</v-icon>
+            <v-btn dark dense v-for="(link, index) in links" :key="index" icon :href="link.url" target="_blank">
+              <v-icon small>{{ link.icon }}</v-icon>
             </v-btn>
           </v-card-actions>
         </v-card>
@@ -26,11 +22,27 @@ export default {
   name: "Footer",
   data() {
     return {
-      links: ["mdi-github", "mdi-heart", "mdi-twitter"],
+      links: [
+        {
+          icon: "mdi-github",
+          url: "https://github.com/Arnman16",
+        },
+        {
+          icon: "mdi-heart",
+          url: "https://aaronnoseworthy.com",
+        },
+        {
+          icon: "mdi-twitter",
+          url: "https://twitter.com/AaronNoseworthy",
+        },
+        {
+          icon: "mdi-linkedin",
+          url: "https://www.linkedin.com/in/aaron-noseworthy",
+        },
+      ],
     };
   },
 };
 </script>
 
-<style>
-</style>
+<style></style>
